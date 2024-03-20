8bce9edc8b218f06bef7c61198bf559b0ad8f46c
Allan THOMAS <allan.thomas.52240@gmail.com>
2024-03-20 20:07:22 +0100

    code import

---
+++ javascript/index1.js
@@ -0,0 +1,36 @@
+﻿
+function nbPatient() {
+	var nb = Math.floor(Math.random() * 1001);
+	
+	alert("Le numéro du patient est " + nb);
+}
+
+
+
+
+function hideElem() {
+    document.getElementById("myDIV").style.visibility = "hidden"; 
+}
+
+function showElem() {
+    document.getElementById("myDIV").style.visibility = "visible"; 
+}
+
+
+
+
+function hideElement() {
+    document.getElementById("tabcomb").style.visibility = "hidden"; 
+}
+
+function showElement() {
+    document.getElementById("tabcomb").style.visibility = "visible"; 
+}
+
+function hideElement2() {
+    document.getElementById("tabcomb2").style.visibility = "hidden"; 
+}
+
+function showElement2() {
+    document.getElementById("tabcomb2").style.visibility = "visible"; 
+}
